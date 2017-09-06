53d396999565eb241466628a0b32fbd603e5e474
Anthony-g2 <ant.greenwell2@gmail.com>
2017-09-06 15:01:34 -0700

    bug fixes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,36 +1,36 @@
 
 
-$(document).ready(function() {
-  console.log('app.js loaded!');
-
-  $.ajax({
-    method: 'GET',
-    url: '#',
-    json: true,
-  })
-  .then(function(restaurants){
-    for(let i=0;i<restaurants.length;i++){
-      $('#restaurants').append(renderRestaurant(restaurants[i]));
-    }
-    $('.form-horizontal').submit(function(event){
-      event.preventDefault();
-      var cereal = $(this).serialize();
-      console.log(cereal);
-      $.ajax({
-        method: 'POST',
-        url:'/api#ants',
-        data: cereal,
-      })
-      .then(function(data){
-        $('#results').append(renderRestaurant(data));
-      })
-    })
-  })
-})
+// $(document).ready(function() {
+//   console.log('app.js loaded!');
+//
+//   $.ajax({
+//     method: 'GET',
+//     url: '#',
+//     json: true,
+//   })
+//   .then(function(restaurants){
+//     for(let i=0;i<restaurants.length;i++){
+//       $('#restaurants').append(renderRestaurant(restaurants[i]));
+//     }
+//     $('#').submit(function(event){
+//       event.preventDefault();
+//       var cereal = $(this).serialize();
+//       console.log(cereal);
+//       $.ajax({
+//         method: 'POST',
+//         url:'#',
+//         data: cereal,
+//       })
+//       .then(function(data){
+//         $('#results').append(renderRestaurant(data));
+//       })
+//     })
+//   })
+// })
 
 function renderRestaurant(restaurant) {
   console.log('rendering restaurant:', restaurant);
-  return (`   <div class="jumbotron resShow">
+  return (`<div class="jumbotron resShow">
                 <img class="imgRest" src="https://typeset-beta.imgix.net/rehost%2F2016%2F9%2F14%2Ff0f9c1fd-615f-42fe-aa62-3ded5a49d228.jpg" alt="restaurant">
                 <ul>
                   <li>${restaurant.name}</li>
