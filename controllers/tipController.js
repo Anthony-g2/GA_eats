535ceeb112f33ad64ddd9fee29d8266d546af573
mnfmnfm <mnfmnfm@gmail.com>
2017-09-12 11:44:32 -0700

    switch song out for restaurant

--- controllers/tipController.js
+++ controllers/tipController.js
@@ -22,11 +22,11 @@ function destroy(req, res){
   //delete a specific tip by id from a  specific restaurant by id
   db.Restaurant.findById(req.params.tipId, function(err, foundRestaurant) {
     console.log(foundRestaurant);
-    // we've got the tip, now find the song within it
+    // we've got the tip, now find the restaurant within it
     var correctTip = foundRestaurant.tips.id(req.params.tipId);
     if (correctTip) {
       correctTip.remove();
-      // resave the tip now that the song is gone
+      // resave the tip now that the restaurant is gone
       foundRestaurant.save(function(err, saved) {
         console.log('REMOVED ', correctTip.name, 'FROM ', saved.tips);
         res.json(correctTip);
